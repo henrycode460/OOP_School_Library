6a1af7053335409887c0958035e58acd370d1b1f
Henry <dokinruss@gmail.com>
2022-09-06 11:12:12 +0000

    CREATE SETUP FILE AND CREATE THE PERSON CLASS WITH THE REQUIRED VARIABLES

---
+++ libraryInfo.rb
@@ -0,0 +1,10 @@
+class Person
+    def initialize(id, name = nil, age, parent_permission = true)
+        @id = id
+        @name = name
+        @age = age
+        @parent_permission = parent_permission
+
+        
+    end
+end
