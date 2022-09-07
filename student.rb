eaecffcea15bc77828aee569c0c2c9289fea8c21
Henry <dokinruss@gmail.com>
2022-09-07 11:04:42 +0000

    Create all the class for the oop library and add methods with various functionalitie

---
+++ student.rb
@@ -0,0 +1,15 @@
+require_relative 'person'
+class Student < Person
+  attr_reader :classroom
+
+  def initialize(classroom, age, name, parent_permission)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
+
+
