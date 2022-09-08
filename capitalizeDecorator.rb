bdac979a700d2bf0fb1f920a2c8f67d68adb250b
Henry <dokinruss@gmail.com>
2022-09-08 14:44:40 +0000

    ESTABLISH ASSOCIATION BETWEEN THE STUDENT, PERSON, RENTAL, BOOK , AND CLASSROOM CLASSES, ALSO APPLY ADD, GETTER AND SETTER METHOD AMONG TTHESE CLASSES

--- capitalizeDecorator.rb
+++ capitalizeDecorator.rb
@@ -7,10 +7,3 @@ class CapitalizeDecorator < BaseDecorator
     @nameable.correct_name.capitalize
   end
 end
-
-# person = Person.new(22, 'maximilianus')
-#  puts person.correct_name
-#   capitalizedPerson = CapitalizeDecorator.new(person)
-#   puts capitalizedPerson.correct_name
-#  capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-#   puts capitalizedTrimmedPerson.correct_name
